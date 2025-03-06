c4e64dad89d3f77fa103e05c7f6ecdcca4aa1a61
divyaraut17 <divyaraut287@gmail.com>
2025-03-06 20:43:45 +0530

    Create longest common prefix.c

---
+++ longest common prefix.c
@@ -0,0 +1,11 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+        if (strsSize == 0) return "";
+    char *prefix = strs[0];
+    for (int i = 1; i < strsSize; i++) {
+        while (strstr(strs[i], prefix) != strs[i]) {
+            prefix[strlen(prefix) - 1] = '\0';
+            if (strlen(prefix) == 0) return "";
+        }
+    }
+    return prefix;
+}
